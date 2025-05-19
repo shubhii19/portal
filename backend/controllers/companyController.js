e8b7aaa4da64330073a06676b6c018b8dd2b7338
shubhii19 <vishwakarmashubhi80@gmail.com>
2025-05-19 19:29:28 +0530

    Company Controller

---
+++ backend/controllers/companyController.js
@@ -0,0 +1,75 @@
+import { Company } from "../models/companyModel";
+
+export const registerCompanyController = async (req, res) => {
+  try {
+    const { companyName } = req.body;
+    if (!companyName) {
+      return res.status(400).json({
+        message: "Company name is required.",
+        success: false,
+      });
+    }
+    let company = await Company.findOne({ name: companyName });
+    if (company) {
+      return res.status(400).json({
+        message: "You can't register with existing company.",
+        success: false,
+      });
+    }
+    company = await Company.create({
+      name: companyName,
+      userId: req.id,
+    });
+
+    return res.status(201).json({
+      message: "Company registered successfully.",
+      company,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCompanyController = async (req, res) => {
+  try {
+    const userId = req.id; //logged in user id
+    const companies = await Company.find({ userId });
+    if (!companies) {
+      return res.status(404).json({
+        message: "Companies not found.",
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCompanyByIdController = async (req, res) => {
+  try {
+    const companyId = req.params.id;
+    const company = await Company.findById(companyId);
+    if (!company) {
+      return res.status(404).json({
+        message: "Company not found.",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+        company,
+        success:true
+    })
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateCompanyController = async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
--- backend/models/companyModel.js
+++ backend/models/companyModel.js
@@ -4,6 +4,7 @@ const companySchema = mongoose.Schema({
   name: {
     type: String,
     required: true,
+    unique:true,
   },
   description: {
     type: String,
