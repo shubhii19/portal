342058712e82829c049cf771630bd45163970440
shubhii19 <vishwakarmashubhi80@gmail.com>
2025-05-31 20:31:08 +0530

    company setup

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -6,11 +6,12 @@ import {
   updateCompanyController,
 } from "../controllers/companyController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
+import { singleUpload } from "../middleware/multer.js";
 const router = express.Router();
 
 router.post("/register", isAuthenticated, registerCompanyController);
 router.get("/get", isAuthenticated, getCompanyController);
 router.get("/get/:id", isAuthenticated, getCompanyByIdController);
-router.put("/update/:id", isAuthenticated, updateCompanyController);
+router.put("/update/:id", isAuthenticated,singleUpload, updateCompanyController);
 
 export default router;
