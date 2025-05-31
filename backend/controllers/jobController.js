97df7f1ff570b11618aacbcf8ea718fdd111e22f
shubhii19 <vishwakarmashubhi80@gmail.com>
2025-05-31 12:06:39 +0530

    user persist

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -85,7 +85,9 @@ export const getAllJobsController = async (req,res)=>{
 export const getjobByIdController = async (req,res)=>{
   try {
     const jobId = req.params.id;
-    const job = await Job.findById(jobId);
+    const job = await Job.findById(jobId).populate({
+      path:"applications"
+    });
     if(!job){
       return res.status(404).json({
         message:"Jobs not found.",
