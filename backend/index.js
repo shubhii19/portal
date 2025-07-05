d6d772dfa181a536e2379f3f562296e4be590b5b
shubhii19 <vishwakarmashubhi80@gmail.com>
2025-07-05 18:41:08 +0530

    link updated

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
     // origin:'http://localhost:5173',
-    origin:'http://job-portal-ui-pi.vercel.app',
+    origin:'https://portal-ivory-pi.vercel.app/',
     credentials:true
 }
 
