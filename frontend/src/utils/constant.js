39bbc804c131ddf019e0fdb0aea97a5bc1a74d7d
Shubhi Vishwakarma <158501867+shubhii19@users.noreply.github.com>
2025-07-05 17:39:43 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -3,7 +3,7 @@
 // export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = "https://job-portal-b-aldx.onrender.com";
 
 export const USER_API_END_POINT = `${BASE_URL}/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/job`;
