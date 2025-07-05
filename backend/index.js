dac3d5ab9e6fe9127bd46a28d933efacb709cf85
Shubhi Vishwakarma <158501867+shubhii19@users.noreply.github.com>
2025-07-05 18:04:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
     // origin:'http://localhost:5173',
-    origin:'job-portal-ui-pi.vercel.app',
+    origin:'http://job-portal-ui-pi.vercel.app',
     credentials:true
 }
 
