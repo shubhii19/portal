44f5c937fc831e52c5cff9a9ff895113cb7dfa72
shubhii19 <vishwakarmashubhi80@gmail.com>
2025-07-06 13:12:48 +0530

    url in backend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin:'portal-ivory-pi.vercel.app',
     // origin:'https://portal-ivory-pi.vercel.app',
     credentials:true
 }
